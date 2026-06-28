098558641865c2dc7a4248d023cfb8ea05ff293b
Vlad <2gamerkoe@gmail.com>
2026-06-28 14:24:20 -0300

    WhyChooseZernyatko DONE

--- src/components/WhyChooseZernyatko.jsx
+++ src/components/WhyChooseZernyatko.jsx
@@ -1,24 +1,25 @@
+import css from "../css/whyChooseZernyatko.module.css"
 import { IoSunnyOutline, IoCalendarClearOutline } from "react-icons/io5";
 import { MdOutlineCoffee } from "react-icons/md";
 export default function WhyChooseZernyatko() {
     return (
-        <div>
-            <h2>Чому варто обрати Зернятко?</h2>
-            <ul>
-                <li>
-                    <IoSunnyOutline/>
-                    <h3>Смак і якість</h3>
-                    <p>Лише відібрані кавові зерна та товари від перевірених виробників.</p>
+        <div class="app">
+            <h2 className={css.whyChooseZernyatkoTitle}>Чому варто обрати Зернятко?</h2>
+            <ul className={css.whyChooseZernyatkoList}>
+                <li className={css.whyChooseZernyatkoItem}>
+                    <IoSunnyOutline className={css.whyChooseZernyatkoItemSymbol}/>
+                    <h3 className={css.whyChooseZernyatkoItemTitle}>Смак і якість</h3>
+                    <p className={css.whyChooseZernyatkoItemText}>Лише відібрані кавові зерна та товари від <br /> перевірених виробників.</p>
                 </li>
-                <li>
-                    <MdOutlineCoffee/>
-                    <h3>Усе для кавового ритуалу</h3>
-                    <p>Від турки до кавомолки — ми зібрали повний набір для ідеальної чашки.</p>
+                <li className={css.whyChooseZernyatkoItem}>
+                    <MdOutlineCoffee className={css.whyChooseZernyatkoItemSymbol}/>
+                    <h3 className={css.whyChooseZernyatkoItemTitle}>Усе для кавового ритуалу</h3>
+                    <p className={css.whyChooseZernyatkoItemText}>Від турки до кавомолки — ми зібрали повний набір <br /> для ідеальної чашки.</p>
                 </li>
-                <li>
-                    <IoCalendarClearOutline/>
-                    <h3>Натхнення щодня</h3>
-                    <p>Кава, яка підкреслить твій настрій та стане головною частиною ранкової звички.</p>
+                <li className={css.whyChooseZernyatkoItem}>
+                    <IoCalendarClearOutline className={css.whyChooseZernyatkoItemSymbol}/>
+                    <h3 className={css.whyChooseZernyatkoItemTitle}>Натхнення щодня</h3>
+                    <p className={css.whyChooseZernyatkoItemText}>Кава, яка підкреслить твій настрій та стане <br /> головною частиною ранкової звички.</p>
                 </li>
             </ul>
         </div>
